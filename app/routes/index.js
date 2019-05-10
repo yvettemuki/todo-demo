4f8acf455093d543db0a964fb1957acf3084e58e
yvettemuki <373628977@qq.com>
2019-05-10 13:10:20 +0800

    little change

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@ import RSVP from 'rsvp';
 
 export default Route.extend({
 	model() {
+		window.console.log("test the model of index");
 		let list = [];
 		let l_list = JSON.parse(localStorage.getItem('todoList'));
 		if(l_list) {
