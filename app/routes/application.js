5d849c9c4b7a4dbbdfe2141588cfd86310a771fe
yvettemuki <373628977@qq.com>
2019-05-10 13:11:12 +0800

    little change of the test

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 	beforeModel(){
+		window.console.log("before the model test!");
 		this.transitionTo('index');
 	},
 	model() {
